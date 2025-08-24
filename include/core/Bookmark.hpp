c2916447d2404ddb36297d3502a3ecfba44e8190
dimbo4ka <dimbo4kait@gmail.com>
2025-08-24 20:47:00 +0300

    fix: correct Bookmark and Bookmark::Builder

--- include/core/Bookmark.hpp
+++ include/core/Bookmark.hpp
@@ -5,39 +5,35 @@
 #include <vector>
 
 class Bookmark {
-private:
-    class Builder;
-
 public:
-    Bookmark(Builder builder);
+    class Builder;
 
     const std::string& title() const;
     const std::string& url() const;
-    const std::vector<std::string> tags() const;
-    std::size_t id() const;
-
+    const std::vector<std::string>& tags() const;
+    
 private:
     std::string title_;
     std::string url_;
     std::vector<std::string> tags_;
-    std::size_t id_;
+    
+    Bookmark(Builder builder);
 
+public:
     class Builder {
     public:
         Builder& SetTitle(std::string title);
         Builder& SetUrl(std::string url);
         Builder& SetTags(std::vector<std::string> tags);
-        Builder& SetId(std::size_t id);
+        Bookmark build();
 
         std::string& title();
         std::string& url();
         std::vector<std::string>& tags();
-        std::size_t id();
 
     private:
         std::string title_;
         std::string url_;
         std::vector<std::string> tags_;
-        std::size_t id_;
     };
 };
